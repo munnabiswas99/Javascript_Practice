0abb15c996aa75e87c6ce77d9772388130088b51
munnabiswas99 <biswas15-5261@diu.edu.bd>
2024-05-04 17:44:09 +0600

    Initial commit

---
+++ First.js
@@ -0,0 +1,18 @@
+var name = "Munna Biswas"
+var age = 22
+var weight = 62.5
+
+console.log(name)
+console.log(age, weight)
+
+var a = 10
+var b = 20
+
+a+=5
+
+console.log(a+b)
+
+var x = "Hello"
+var y = ' Javascript'
+console.log(x+y)
+
