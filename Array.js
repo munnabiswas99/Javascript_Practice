2e001513bcacbdab71eb69c064f95dfe1ff19d9e
munnabiswas99 <biswas15-5261@diu.edu.bd>
2024-09-01 21:17:44 +0600

    Array

---
+++ Array.js
@@ -0,0 +1,17 @@
+var age = [22, 25, 30, 35, 40]
+console.log(age)
+
+console.log(age[0])
+
+age[1] = 23
+console.log(age)
+
+var idx = age.indexOf(35)
+console.log(idx)
+
+age.push(45)
+age.push(50)
+console.log(age)
+
+age.pop()
+console.log(age)
