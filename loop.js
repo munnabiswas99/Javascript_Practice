dee9814beb60eaab2e0b19d38d5776384bb441e6
munnabiswas99 <biswas15-5261@diu.edu.bd>
2024-09-02 21:08:07 +0600

    Loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+var arr = [10, 20, 30, 40, 50, 60]
+var i = 0
+
+while(i<6){
+    console.log(arr[i])
+    i++
+}
+
+var arr2 = ['Munna', 'Biswas', 'Afser', 'Habib', 'Sayem']
+
+for(var i = 0; i<arr2.length; i++){
+    console.log(arr2[i])
+}
